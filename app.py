b58a0f023d90e0ef485f51e1aade409bb9dcce44
Bryan <bryanlu.my@gmail.com>
2024-11-01 10:49:51 +0800

    changed to https

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 from chat_routes import chat_bp
-
-import torch
 
 
 # App stuff
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(chat_bp, url_prefix='/api/chat')
 
 
-@app.route('/', methods=['GET'])
+@ app.route('/', methods=['GET'])
 def home():
     return "Python Flask Server is running.", 200
 
 
 if __name__ == '__main__':
     # Run the Flask app on port 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context=(
+        'server.cert', 'server.key'))
